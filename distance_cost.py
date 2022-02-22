ef62cd65752809178ee65497a3adbb3513ec5e2b
betrevisan <bernardo.trevisan@gmail.com>
2022-02-22 06:47:52 +0000

    Implement the qubo formulation with the distance cost

--- distance_cost.py
+++ distance_cost.py
@@ -3,17 +3,27 @@
 
 from dwave.system import EmbeddingComposite, DWaveSampler
 
-distance = 250
+distance = 470
 total_distance = 500
 d = distance/total_distance
 # Can be tuned to change how much influence distance has on attention allocation
 constant = 80
 
 # QUBO Matrix
-Q = {('50','50'): d - 1,
-    ('75','75'): -d,
-    ('50','75'): 1}
-
+# Q = {('50','50'): d - 1,
+#     ('75','75'): -d,
+#     ('50','75'): 1}
+Q = {('25','25'): d - 1,
+    ('50','50'): 0.5*d - 0.9,
+    ('75','75'): -0.5*d - 0.4,
+    ('100','100'): -d,
+    ('25','50'): -(d - 1 + 0.5*d - 0.9),
+    ('25','75'): -(d - 1 - 0.5*d - 0.4),
+    ('25','100'): -(d - 1 - d),
+    ('50','75'): -(0.5*d - 0.9 - 0.5*d - 0.4),
+    ('50','100'): -(0.5*d - 0.9 - d),
+    ('75','100'): -(-0.5*d - 0.4 - d)}
+    
 # Define sampler and run it
 sampler = EmbeddingComposite(DWaveSampler())
 
